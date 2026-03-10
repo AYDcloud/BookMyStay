d6c23a1ad46e56aa23564b2478058a9b8654bdfa
AYDcloud <ayushdalela4903@gmail.com>
2026-03-10 23:56:36 +0530

    Initial commit - BookMyStay application entry point

---
+++ src/BookMyStay.java
@@ -0,0 +1,9 @@
+public class BookMyStay {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to BookMyStay!");
+        System.out.println("Application: BookMyStay Hotel Booking System");
+        System.out.println("Version: 1.0");
+    }
+
+}
