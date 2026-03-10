8e98d0ece70d070e3299b669662bb4d92f4f33a8
AYDcloud <ayushdalela4903@gmail.com>
2026-03-11 00:01:37 +0530

    Implement UC2

--- src/BookMyStay.java
+++ src/BookMyStay.java
@@ -1,9 +1,63 @@
+abstract class Room {
+
+    protected String type;
+    protected int beds;
+    protected double price;
+
+    public Room(String type, int beds, double price) {
+        this.type = type;
+        this.beds = beds;
+        this.price = price;
+    }
+
+    public void displayDetails() {
+        System.out.println("Room Type: " + type);
+        System.out.println("Beds: " + beds);
+        System.out.println("Price per night: $" + price);
+    }
+}
+
+class SingleRoom extends Room {
+    public SingleRoom() {
+        super("Single Room", 1, 100);
+    }
+}
+
+class DoubleRoom extends Room {
+    public DoubleRoom() {
+        super("Double Room", 2, 180);
+    }
+}
+
+class SuiteRoom extends Room {
+    public SuiteRoom() {
+        super("Suite Room", 3, 350);
+    }
+}
+
 public class BookMyStay {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to BookMyStay!");
-        System.out.println("Application: BookMyStay Hotel Booking System");
-        System.out.println("Version: 1.0");
-    }
 
+        Room single = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suite = new SuiteRoom();
+
+        int singleAvailability = 5;
+        int doubleAvailability = 3;
+        int suiteAvailability = 2;
+
+        System.out.println("Available Rooms\n");
+
+        single.displayDetails();
+        System.out.println("Available: " + singleAvailability);
+        System.out.println();
+
+        doubleRoom.displayDetails();
+        System.out.println("Available: " + doubleAvailability);
+        System.out.println();
+
+        suite.displayDetails();
+        System.out.println("Available: " + suiteAvailability);
+    }
 }
